5e30232004781a12eaf1f55a4403e8f961f0d8d3
4prndz <victorkouichi40@gmail.com>
2024-09-25 02:06:33 -0300

    feat: button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+	function open() {
+		alert('hello world');
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={open}>Open</button>
+
+<style>
+	button {
+		background: orange;
+		color: white;
+	}
+</style>
