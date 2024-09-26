0db5d8a04805a405f8eba73a65d5a6d1c2c5ee55
4prndz <victorkouichi40@gmail.com>
2024-09-25 22:26:06 -0300

    feat: svelte:head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
 	import '$lib/global.css';
 </script>
 
+<svelte:head>
+	<title>Product</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Palanquin+Dark&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <slot />
